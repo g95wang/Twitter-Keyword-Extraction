e11f2dab9991ce0fcb51da9df27ad199e92d2e40
Kevin <g95wang@gmail.com>
2020-11-17 22:32:12 -0500

    Remove unnecessary files

--- tf_crf_layer/__init__.py
+++
@@ -1,5 +0,0 @@
-from tf_crf_layer import layer
-from tf_crf_layer import loss
-from tf_crf_layer import metrics
-
-__version__ = "0.2.5"
--- tf_crf_layer/loss/__init__.py
+++
@@ -1,1 +0,0 @@
-from tf_crf_layer.loss.crf_loss import crf_loss, ConditionalRandomFieldLoss
